f372685f49e55736df81e1455dc6ddad8929fc81
xTorch8 <esantosa.es@gmail.com>
2025-05-01 12:36:10 +0700

    F4-Sprint2-TranscriptionSummarization - Update entities datatypes

--- databases/tr_verification_token.py
+++ databases/tr_verification_token.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, Relationship, Text
+from sqlmodel import Column, Field, Relationship, Text
 from databases.timestamp_mixin import TimestampMixin
 from datetime import datetime
 # from databases.lt_verification_type import LtVerificationType
@@ -8,7 +8,7 @@ class TrVerificationToken(TimestampMixin, table = True):
     __table_args__ = {"extend_existing": True}
 
     verificationTokenID: int = Field(default = None, primary_key = True)
-    token: str = Field(sa_column = Text)
+    token: str = Field(sa_column = Column(Text))
     expires: datetime
     verificationTypeID: int = Field(foreign_key = "LtVerificationType.verificationTypeID")
 
