73752867c79f199d71d65dba8f9636ec8db8187a
xTorch8 <esantosa.es@gmail.com>
2025-05-01 12:36:52 +0700

    F4-Sprint2-TranscriptionSummarization - Update seed_database

--- databases/seed_database.py
+++ databases/seed_database.py
@@ -1,8 +1,16 @@
 from sqlmodel import SQLModel, Session
 from databases.database import engine
 from databases.lt_tools import LtTools
+from databases.lt_role import LtRole
+from databases.ms_user import MsUser
+from dotenv import load_dotenv
+import uuid
+import bcrypt
+import os
 
 def seed_database():
+    load_dotenv()
+
     with Session(engine) as session:
         tools = [
             LtTools(
@@ -15,7 +23,29 @@ def seed_database():
             )
         ]
 
-        session.add_all(tools)
+        roles = [
+            LtRole(
+                roleID = 1,
+                name = "User"
+            ),
+            LtRole(
+                role = 2,
+                name = "Admin"
+            )
+        ]
+
+        users = [
+            MsUser(
+                userID = str(uuid.uuid4()),
+                name = "Admin",
+                email = os.getenv("ADMIN_EMAIL"),
+                password = bcrypt.hashpw(os.getenv("ADMIN_PASSWORD").encode("utf-8"), bcrypt.gensalt()),
+                roleID = 2,
+                isVerified = True
+            )
+        ]
+
+        session.add_all(tools + roles + users)
         session.commit()
         print("Successfully seed database")
 
