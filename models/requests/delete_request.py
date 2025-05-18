c5afc35fddb0c2fb26231873abaeea18e4a93231
xTorch8 <esantosa.es@gmail.com>
2025-05-18 17:32:58 +0700

    F5-Sprint2-UserDashboard - Fix dashboard API

--- models/requests/delete_request.py
+++ models/requests/delete_request.py
@@ -2,4 +2,5 @@ from typing import List
 from pydantic import BaseModel, Field
 
 class DeleteRequest(BaseModel):
-    workspaceID: List[int] = Field(..., alias="workspaceID")
+    workspaceID: List[str] = Field(..., alias="workspaceID")
+    userID: str
